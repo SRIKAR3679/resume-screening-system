3b8cf7dbd2b954e0327bca5ba6da95c38389ffa9
Srikar <srikarreddyganda@gmail.com>
2026-08-18 15:37:24 +0530

    Configure GitHub Pages deployment - add base path and gh-pages script

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,10 @@ import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/resume-screening-system">
       <App />
       <Toaster position="top-right" />
     </BrowserRouter>
   </React.StrictMode>
 )
+
